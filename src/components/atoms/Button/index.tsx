a4d2033a11981f9af6d58be94b1052e2ac55da96
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-08 20:18:58 -0300

    feat: component button

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import clsx from "clsx";
+import { ButtonProps } from "@/components/atoms/Button/type";
+
+const Button: FC<ButtonProps> = ({
+  children,
+  onClick,
+  type = "button",
+  className,
+  id,
+  name,
+  disabled,
+  default: isDefault =false,
+  ...props
+}) => {
+  return (
+    <button
+      id={id}
+      name={name}
+      className={clsx(
+        !isDefault && "bg-primary-color hover:bg-secondary-color py-2 px-4 rounded text-white text-base transition-colors duration-300 focus:outline-none",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        className
+      )}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
