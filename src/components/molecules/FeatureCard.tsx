f4b5764afd3005b9b62dd49b0afb5bdc53b74e11
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-08 20:39:28 -0300

    refactor: class

--- src/components/molecules/FeatureCard.tsx
+++ src/components/molecules/FeatureCard.tsx
@@ -10,9 +10,9 @@ interface FeatureCardProps {
 const FeatureCard: React.FC<FeatureCardProps> = ({ icon, title, description }) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-6 flex flex-col gap-4">
-       <Icon icon={icon} size='text-4xl'/>
-      <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
-      <p className="text-gray-600">{description}</p>
+       <Icon icon={icon}/>
+      <h3 className="text-xl font-semibold text-text-color">{title}</h3>
+      <p className="text-text-color">{description}</p>
     </div>
   );
 };
