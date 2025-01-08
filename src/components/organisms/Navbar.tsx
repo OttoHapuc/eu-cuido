866337fd55922bf48bf29fcdb35d529db1464b1e
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-08 20:40:55 -0300

    refactor: recreate and use new model components

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Logo from '@/components/atoms/Logo';
-import AuthButtons from '@/components/molecules/AuthButtons';
+import React from "react";
+import AuthButtons from "@/components/molecules/AuthButtons";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-white border-b border-gray-200 py-4 px-6 flex justify-between items-center">
-      <Logo />
+      <Link href="/" className={`font-bold text-2xl text-text-color`}>
+        Eu Cuido
+      </Link>
       <AuthButtons />
     </nav>
   );
