9695250f9852622471b4aa7c7b52c26f90eadf70
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-08 20:39:03 -0300

    refactor: reestructure actions and semantic

--- src/components/molecules/AuthButtons.tsx
+++ src/components/molecules/AuthButtons.tsx
@@ -1,26 +1,25 @@
-'use client'
-import React from 'react';
-import Button from '@/components/atoms/Button';
-import Link from 'next/link';
-import { useAuth } from '@/context/AuthContext';
+"use client";
+import React from "react";
+import Button from "@/components/atoms/Button";
+import { useAuth } from "@/context/AuthContext";
+import { useModal } from "@/context/ModalProvider";
+import Access from "../organisms/Access";
 
 const AuthButtons: React.FC = () => {
   const { isAuthenticated, logout } = useAuth();
+  const { showModal } = useModal();
+
+  const handleOpenLoginModal = () => {
+    showModal(<Access />);
+  };
 
   return (
     <div className="flex items-center gap-4">
       {isAuthenticated ? (
-         <Button variant='secondary' onClick={logout}>Logout</Button>
+        <Button onClick={logout}>Logout</Button>
       ) : (
-        <>
-            <Link href="/login">
-               <Button variant="link">Entrar</Button>
-            </Link>
-            <Link href="/register">
-                <Button>Cadastrar</Button>
-            </Link>
-        </>
-       )}
+        <Button onClick={handleOpenLoginModal}>Acessar</Button>
+      )}
     </div>
   );
 };
