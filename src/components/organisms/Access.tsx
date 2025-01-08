3318ae57ce5c26213c4b41cf05f925631d1e5a82
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-08 20:17:24 -0300

    feat: component methos access

---
+++ src/components/organisms/Access.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import Login from "../molecules/Login";
+import Register from "../molecules/Register";
+import Button from "../atoms/Button";
+
+const Access: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <>
+      {isLogin ? <Login /> : <Register />}
+      <Button onClick={() => setIsLogin(!isLogin)} default={true} className="mt-4 hover:underline underline-offset-2 secondary-text">
+        {isLogin ? "Possuo um acesso" : "Realizar registro"}
+      </Button>
+    </>
+  );
+};
+
+export default Access;
