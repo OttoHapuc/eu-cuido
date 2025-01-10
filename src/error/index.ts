e72ccf7af09ec70344c3a7d0a65d061aa718e433
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-09 22:59:57 -0300

    feat: custon error

---
+++ src/error/index.ts
@@ -0,0 +1,9 @@
+export * from './CustomError';
+
+export * from './errors/NotFound';
+export * from './errors/NotImplemented';
+export * from './errors/NotAcceptable';
+export * from './errors/Unauthorized';
+export * from './errors/UnprocessableEntity';
+export * from './errors/FailedDependency';
+export * from './errors/MethodNotAllowed';
