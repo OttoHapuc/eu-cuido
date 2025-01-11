5e7b37b9e375baba16f0a665bc42f005b7aaee99
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:10:29 -0300

    feat: umprocessable entity error

---
+++ src/error/errors/UnprocessableEntity.ts
@@ -0,0 +1,12 @@
+import { CustomError } from '../CustomError';
+
+export class UnprocessableEntity extends CustomError {
+    constructor(
+        message: string = 'Unprocessable Entity',
+        errorCode: number = 422,
+        originalError?: Error,
+        public issues?: []
+    ) {
+        super(message, 422, errorCode, originalError);
+    }
+}
