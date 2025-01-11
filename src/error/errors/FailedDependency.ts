a6581d68c92da8efd1b400596578fca98f0d7540
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:08:22 -0300

    feat: failed dependency error

---
+++ src/error/errors/FailedDependency.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class FailedDependency extends CustomError {
+    constructor(
+        message: string = 'Failed Dependency',
+        errorCode: number = 424,
+        originalError?: Error,
+    ) {
+        super(message, 424, errorCode, originalError);
+    }
+}
