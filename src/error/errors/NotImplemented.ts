930adee5772a3707632c12e51d19c85953bf181b
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:09:35 -0300

    feat: not implemented error

---
+++ src/error/errors/NotImplemented.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class NotImplemented extends CustomError {
+    constructor(
+        message: string = 'Not Implemented',
+        errorCode: number = 501,
+        originalError?: Error,
+    ) {
+        super(message, 501, errorCode, originalError);
+    }
+}
