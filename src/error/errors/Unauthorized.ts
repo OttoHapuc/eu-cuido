374a6e5f04cfb7750bd3f6e168f0921fb4498da3
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:09:49 -0300

    feat: unauthorized error

---
+++ src/error/errors/Unauthorized.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class Unauthorized extends CustomError {
+    constructor(
+        message: string = 'Unauthorized',
+        errorCode: number = 401,
+        originalError?: Error,
+    ) {
+        super(message, 401, errorCode, originalError);
+    }
+}
