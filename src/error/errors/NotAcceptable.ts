5eaa051a00d94aff21d67961310c103d803eaad2
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:09:05 -0300

    feat: not acceptable error

---
+++ src/error/errors/NotAcceptable.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class NotAcceptable extends CustomError {
+    constructor(
+        message: string = 'Not Acceptable',
+        errorCode: number = 406,
+        originalError?: Error,
+    ) {
+        super(message, 406, errorCode, originalError);
+    }
+}
