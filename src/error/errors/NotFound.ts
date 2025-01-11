8e3f6cc13de7e6eb9f35c63fe709882b995bd407
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:09:20 -0300

    feat: not found error

---
+++ src/error/errors/NotFound.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class NotFound extends CustomError {
+    constructor(
+        message: string = 'Not Found',
+        errorCode: number = 404,
+        originalError?: Error,
+    ) {
+        super(message, 404, errorCode, originalError);
+    }
+}
