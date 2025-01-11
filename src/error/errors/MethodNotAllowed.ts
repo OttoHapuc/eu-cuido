67ddab518fdf252f6a6edfc2fbcca31f31e3b642
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-10 21:08:50 -0300

    feat: method not allowed

---
+++ src/error/errors/MethodNotAllowed.ts
@@ -0,0 +1,11 @@
+import { CustomError } from '../CustomError';
+
+export class MethodNotAllowed extends CustomError {
+    constructor(
+        message: string = 'Method Not Allowed',
+        errorCode: number = 405,
+        originalError?: Error,
+    ) {
+        super(message, 405, errorCode, originalError);
+    }
+}
