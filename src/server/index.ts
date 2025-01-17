3b4a9f9586a8bdfc1044c6f9743f3827c85708b5
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-16 23:24:55 -0300

    feat: export padronized path

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,3 +3,6 @@ export {
     registerController,
     meController,
 } from './modules/auth/auth.controller';
+export {
+    getOnboardingDataController,
+} from './modules/onboarding/onboarding.controller';
