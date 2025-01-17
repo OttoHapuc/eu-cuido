551918aaa7a39f745a39170dd3a94d871cb3e03c
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-16 23:19:55 -0300

    refactor: add internal server error

--- src/error/index.ts
+++ src/error/index.ts
@@ -6,4 +6,5 @@ export * from './errors/NotAcceptable';
 export * from './errors/Unauthorized';
 export * from './errors/UnprocessableEntity';
 export * from './errors/FailedDependency';
+export * from './errors/InternalServerError';
 export * from './errors/MethodNotAllowed';
