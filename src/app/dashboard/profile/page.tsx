1cfda9f2eff369d42293788e001f5e58684dd7bb
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-18 00:41:09 -0300

    refactor: profile initial form steps saves

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,6 +1,29 @@
 'use client';
+import React from 'react';
+import Text from '@/components/atoms/Text';
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
+import Button from '@/components/atoms/Button';
+import UserRoleForm from '@/components/molecules/UserRoleForm';
+import AddressForm from '@/components/molecules/AddressForm';
 
-const Home: React.FC = () => {
-    return <>hello</>;
+const ProfilePage: React.FC = () => {
+    const { user } = useAuth();
+    const router = useRouter();
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <Text type="h1" className="mb-8">
+                Perfil
+            </Text>
+            <div className="flex flex-col gap-4 w-[800px]">
+                <Text type="h2">Dados de Role</Text>
+                <UserRoleForm />
+                <Text type="h2">Dados de Endereço</Text>
+                <AddressForm />
+             </div>
+        </div>
+    );
 };
-export default Home;
+
+export default ProfilePage;
