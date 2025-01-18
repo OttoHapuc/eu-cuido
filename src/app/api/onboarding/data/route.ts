336c21e6ef17d07d51ae3fa97e9f65ed34c96290
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-18 10:21:50 -0300

    delet: not a viable system

--- src/app/api/onboarding/data/route.ts
+++
@@ -1,6 +0,0 @@
-import { getOnboardingDataController } from '@/server';
-import { NextRequest } from 'next/server';
-
-export async function GET(req: NextRequest) {
-    return await getOnboardingDataController(req);
-}
