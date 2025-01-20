309e106340facec3909b52241b1a5de52344c1e9
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-19 23:27:18 -0300

    refactor: types functions and exract authorization user in private rotes

--- src/server/modules/user/user.service.ts
+++ src/server/modules/user/user.service.ts
@@ -1,41 +1,46 @@
 import { CustomError, Unauthorized } from '@/error';
-import { JwtPayload } from 'jsonwebtoken';
-import jwt from 'jsonwebtoken';
 import { userRepository } from './user.repository';
+import { userTransform } from './user.transform';
+import { UserAdressDTO, UserRolesDTO } from './user.types';
 
 const jwtSecret = process.env.JWT_SECRET || 'secret';
 
-export const getUserRoles = async (authHeader: string | null) => {
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Unauthorized('Token não fornecido');
+export const authenticatedUser= async (id: number) => {
+    const user = await userRepository.findById(id);
+    if (!user) {
+        throw new Unauthorized('User not found');
     }
-    const token = authHeader.split(' ')[1];
+    return user.id
+}
+
+export const user = async (id: number) => {
     try {
-        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
-        const user = await userRepository.findById(decoded.id);
+        const user = await userRepository.findById(id);
         if (!user) {
             throw new Unauthorized('Usuário não encontrado');
         }
-        return {user_roles: await userRepository.getUserRoles(user.id), roles: await userRepository.getRoles()};
+
+        return userTransform(user);
+    } catch (error: any) {
+        throw new CustomError(error);
+    }
+};
+
+export const getUserRoles = async (id: number) => {
+    try {
+        return {user_roles: await userRepository.getUserRoles(id), roles: await userRepository.getRoles()};
     } catch (error: any) {
         throw new Unauthorized('Token inválido');
     }
 }
 
 export const saveUserRoleService = async (
-    data: { roles: number[] },
-    authHeader: string | null
+    id: number,
+    data: UserRolesDTO,
 ) => {
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Unauthorized('Token não fornecido');
-    }
-
-    const token = authHeader.split(' ')[1];
-
     try {
-        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
 
-        const user = await userRepository.findById(decoded.id);
+        const user = await userRepository.findById(id);
         if (!user) {
             throw new Unauthorized('Usuário não encontrado');
         }
@@ -56,60 +61,30 @@ export const saveUserRoleService = async (
     }
 };
 
-export const getAddressByUserId = async (authHeader: string | null) => {
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Unauthorized('Token não fornecido');
-    }
-    const token = authHeader.split(' ')[1];
+export const getAddressByUserId = async (id: number) => {
     try {
-        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
-        const user = await userRepository.findById(decoded.id);
-        if (!user) {
-            throw new Unauthorized('Usuário não encontrado');
-        }
-        return userRepository.getAddressByUserId(user.id);
+        return await userRepository.getAddressByUserId(id);
     } catch (error: any) {
         throw new Unauthorized('Token inválido');
     }
 }
 
 export const saveAddressService = async (
-    data: {
-        id: string;
-        street: string;
-        number: string;
-        complement?: string;
-        neighborhood: string;
-        city: string;
-        state: string;
-        zip_code: string;
-    }[],
-    authHeader: string | null
+    data: UserAdressDTO[],
+    user_id: number
 ) => {
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Unauthorized('Token não fornecido');
-    }
-
-    const token = authHeader.split(' ')[1];
-
     try {
-        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
 
-        const user = await userRepository.findById(decoded.id);
-        if (!user) {
-            throw new Unauthorized('Usuário não encontrado');
-        }
-
-        await userRepository.deleteUserAddress(user.id);
+        await userRepository.deleteUserAddress(user_id);
 
         data.forEach( async d => {
             const {id, ...res} = d;
             await userRepository.createAddress({
-            user_id: user.id,
+            user_id: user_id,
             ...res,
         })});
 
-        return user;
+        return {success: true};
     } catch (error: any) {
         throw new CustomError(error);
     }
