05bb79722fd81b0c979a17e7c98747dbfd5c4557
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-19 23:29:38 -0300

    feat: user transform data

---
+++ src/server/modules/user/user.transform.ts
@@ -0,0 +1,12 @@
+import { User } from './user.types';
+
+export const userTransform = (user: User): Omit<User, 'password'> => {
+    return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        status: user.status,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+    };
+};
