10e30f4bd4050f9e67343a876abf28104f3d308b
OttoHapuc <dossantoshapuc@gmail.com>
2025-01-19 23:29:58 -0300

    refactor: extract method deprecated

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,3 @@ export {
     registerController,
     meController,
 } from './modules/auth/auth.controller';
-export {
-    getOnboardingDataController,
-} from './modules/onboarding/onboarding.controller';
